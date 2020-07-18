a4ccf37b6be256166c7bfd8b93d6db0e6dcfb517
Mohamed Magdy <md.magdy.sa@gmail.com>
2020-07-18 08:08:51 +0200

    Removing unused imports at the free version

--- packages/block-library/src/index.js
+++ packages/block-library/src/index.js
@@ -4,43 +4,27 @@ import {
 } from '@quillforms/blocks';
 import '@wordpress/element';
 
-import * as welcomeScreen from './welcome-screen';
 import * as website from './website';
-import * as thankyouScreen from './thankyou-screen';
 import * as statement from './statement';
 import * as shortText from './short-text';
-import * as rating from './rating';
-import * as pictureChoice from './picture-choice';
-import * as phone from './phone';
-import * as opinionScale from './opinion-scale';
 import * as number from './number';
 import * as multipleChoice from './multiple-choice';
 import * as longText from './long-text';
-import * as legal from './legal';
-import * as file from './file';
 import * as email from './email';
 import * as dropdown from './dropdown';
 import * as date from './date';
 
 export const setBlocksRendererSettings = () => {
 	[
-		welcomeScreen,
 		website,
-		thankyouScreen,
 		statement,
 		shortText,
-		rating,
-		pictureChoice,
-		phone,
 		longText,
 		dropdown,
 		email,
-		file,
 		multipleChoice,
 		number,
-		opinionScale,
 		date,
-		legal,
 	].forEach( ( block ) => {
 		setBlockRendererConfig( block.type, {
 			...block.settings.rendererConfig,
@@ -50,23 +34,15 @@ export const setBlocksRendererSettings = () => {
 
 export const setBlocksEditorSettings = () => {
 	[
-		welcomeScreen,
 		website,
-		thankyouScreen,
 		statement,
 		shortText,
-		rating,
-		pictureChoice,
-		phone,
 		longText,
 		dropdown,
 		email,
-		file,
 		multipleChoice,
 		number,
-		opinionScale,
 		date,
-		legal,
 	].forEach( ( block ) => {
 		setBlockEditorConfig( block.type, {
 			...block.settings.editorConfig,
