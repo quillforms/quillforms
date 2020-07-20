17149c5ad5febbbfece28f8eb02d31ed46033c35
Mohamed Magdy <md.magdy.sa@gmail.com>
2020-07-20 05:50:49 +0200

    Fixing the sudden delete of prevfields inserter component

---
+++ packages/block-editor/src/components/prevfields-inserter/index.js
@@ -0,0 +1,58 @@
+/**
+ * QuillForms Dependencies
+ */
+import { Tooltip, RecallInformation } from '@quillforms/builder-components';
+
+/**
+ * WordPress Dependenciesd
+ */
+import { useState } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
+
+const PrevFieldsInserter = ( { onInsert, id, category } ) => {
+	const [ anchorEl, setAnchorEl ] = useState( null );
+
+	const { prevFields } = useSelect( ( select ) => {
+		return {
+			prevFields: select( 'quillForms/builder-core' )
+				.getPreviousEditableFields( id, category )
+				.map( ( field ) => {
+					return {
+						varType: 'field',
+						title: field.title,
+						ref: field.id,
+					};
+				} ),
+		};
+	} );
+
+	return (
+		<div className="block-editor-prev-fields-inserter">
+			<Tooltip
+				title="Recall Information"
+				placement="bottom"
+				arrow={ true }
+			>
+				<div
+					role="presentation"
+					className="block-editor-block-toolbar__controls-icon"
+					onClick={ ( event ) => {
+						setAnchorEl( event.currentTarget );
+					} }
+				>
+					@
+				</div>
+			</Tooltip>
+			<RecallInformation
+				anchorEl={ anchorEl }
+				variables={ prevFields }
+				insertVariable={ onInsert }
+				onClose={ () => {
+					setAnchorEl( null );
+				} }
+				setAnchorEl={ setAnchorEl }
+			/>
+		</div>
+	);
+};
+export default PrevFieldsInserter;
