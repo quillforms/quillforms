8189668e8e4960ff0d8f39ce01b8f245356124b2
mdmag <md.magdy.sa@gmail.com>
2020-09-15 00:16:43 +0200

    Fetching single post data from rest api and saving the data in a global variable so store resolvers will access it

--- client/pages/builder/index.js
+++ client/pages/builder/index.js
@@ -1,12 +1,40 @@
-import './style.scss';
-import { useEffect } from '@wordpress/element';
+/**
+ * QuillForms Dependencies
+ */
 import { EditorProvider } from '@quillforms/builder-core';
 
+/**
+ * WordPress Dependencies
+ */
+import { useEffect, useState } from '@wordpress/element';
+import apiFetch from '@wordpress/api-fetch';
+
+/**
+ * External Dependencies
+ */
+import Loader from 'react-loader-spinner';
+import { css } from 'emotion';
+
+/**
+ * Internal Dependencies
+ */
+import './style.scss';
+
 const Builder = ( { params } ) => {
+	const { id } = params;
+	const [ isFetching, setIsFetching ] = useState( true );
+
 	useEffect( () => {
 		document
 			.getElementsByTagName( 'body' )[ 0 ]
 			.classList.add( 'js', 'is-fullscreen-mode' );
+		apiFetch( {
+			path: `/wp/v2/quill_forms/${ id }`,
+			method: 'GET',
+		} ).then( ( res ) => {
+			window.qfInitialPayload = res;
+			setIsFetching( false );
+		} );
 		return () => {
 			document
 				.getElementsByTagName( 'body' )[ 0 ]
@@ -15,7 +43,27 @@ const Builder = ( { params } ) => {
 	}, [] );
 	return (
 		<div id="quillforms-layout-wrapper">
-			<EditorProvider />
+			{ isFetching ? (
+				<div
+					className={ css`
+						display: flex;
+						flex-wrap: wrap;
+						width: 100%;
+						min-height: 100vh;
+						justify-content: center;
+						align-items: center;
+					` }
+				>
+					<Loader
+						type="Bars"
+						color="#00BFFF"
+						height={ 30 }
+						width={ 30 }
+					/>
+				</div>
+			) : (
+				<EditorProvider />
+			) }
 		</div>
 	);
 };
