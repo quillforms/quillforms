4bae0065eac9f4ae9e9894d2c0789fb3e5ff3730
mdmag <md.magdy.sa@gmail.com>
2020-09-24 02:37:49 +0200

    Adding required asterisk

--- packages/builder-components/src/control-label/index.js
+++ packages/builder-components/src/control-label/index.js
@@ -1,5 +1,25 @@
-const ControlLabel = ( { label } ) => {
-	return <div className="builder-components-control-label">{ label }</div>;
+/**
+ * External Dependencies.
+ */
+import { css } from 'emotion';
+
+const ControlLabel = ( { label, showAsterisk } ) => {
+	return (
+		<div className="builder-components-control-label">
+			{ label }
+			{ showAsterisk && (
+				<span
+					className={ css`
+						color: #d94343;
+						font-size: 16px;
+						margin-left: 1px;
+					` }
+				>
+					*
+				</span>
+			) }
+		</div>
+	);
 };
 
 export default ControlLabel;
