2d0ec0fbc4d5e781cba6a06e8243d5c5205dc618
mdmag <md.magdy.sa@gmail.com>
2020-09-29 07:15:11 +0200

    Initilizing document editor package

---
+++ packages/document-editor/src/store/index.js
@@ -0,0 +1,13 @@
+/**
+ * WordPress Dependencies
+ */
+import { registerStore } from '@wordpress/data';
+import reducer from './reducer';
+import * as actions from './actions';
+import * as selectors from './selectors';
+const store = registerStore( 'quillForms/document-editor', {
+	reducer,
+	actions,
+	selectors,
+} );
+export default store;
