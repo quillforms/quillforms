af45d53d4b077a208a5db3ce021c6f9ea69c6568
mdmag <md.magdy.sa@gmail.com>
2020-11-01 02:22:36 +0200

    Having registerPanels called internally inside builder panels package instead of builder core

--- packages/builder-panels/src/index.js
+++ packages/builder-panels/src/index.js
@@ -2,5 +2,7 @@ import './store';
 // import '@quillforms/theme-editor';
 import '@quillforms/notifications-editor';
 import '@quillforms/messages-editor';
-export { registerPanels } from './panels';
+import { registerPanels } from './panels';
 export * from './api';
+
+registerPanels();
