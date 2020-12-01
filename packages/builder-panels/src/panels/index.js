7807f13ee4a856550b0f2733ae243510eb6cb96f
mdmag <md.magdy.sa@gmail.com>
2020-12-01 23:26:49 +0200

    Adding import for theme-editor package

--- packages/builder-panels/src/panels/index.js
+++ packages/builder-panels/src/panels/index.js
@@ -6,21 +6,27 @@ import { panelSettings as Blocks } from './blocks';
 import { panelSettings as BlockControls } from './block-controls';
 import { panelSettings as Notifications } from './notifications';
 import { panelSettings as Settings } from './settings';
+import { panelSettings as Theme } from './theme';
 
 // Sub Panels
 import { subpanelSettings as Messages } from './settings/subpanels/messages';
 import { subpanelSettings as Document } from './settings/subpanels/document';
+import { subpanelSettings as MyThemes } from './theme/subpanels/my-themes';
+import { subpanelSettings as CustomizeTheme } from './theme/subpanels/customize';
 
 export const registerPanels = () => {
 	// Register Panels
-	[ Blocks, Notifications, BlockControls, Settings ].forEach( ( panel ) => {
-		registerBuilderPanel( panel.name, {
-			...omit( panel, [ 'name' ] ),
-		} );
-	} );
+	[ Blocks, Notifications, BlockControls, Theme, Settings ].forEach(
+		( panel ) => {
+			console.log( panel );
+			registerBuilderPanel( panel.name, {
+				...omit( panel, [ 'name' ] ),
+			} );
+		}
+	);
 
 	// Register Subpanels
-	[ Document, Messages ].forEach( ( subpanel ) => {
+	[ Document, Messages, MyThemes, CustomizeTheme ].forEach( ( subpanel ) => {
 		registerBuilderSubPanel( subpanel.name, {
 			...omit( subpanel, [ 'name' ] ),
 		} );
