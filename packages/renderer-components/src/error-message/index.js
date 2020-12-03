4d385ae56e1dd2f0f07569f1c921572ea17d71fa
mdmag <md.magdy.sa@gmail.com>
2020-12-03 16:46:57 +0200

    Adjusting error message component to match the current theme

--- packages/renderer-components/src/error-message/index.js
+++ packages/renderer-components/src/error-message/index.js
@@ -1,6 +1,26 @@
+/**
+ * External Dependencies
+ */
+import classnames from 'classnames';
+import { css } from 'emotion';
+
+/**
+ * Internal Dependencies
+ */
+import useTheme from '../hooks/use-theme';
+
 const ErrorMsgWrapper = ( { message } ) => {
+	const theme = useTheme();
 	return (
-		<div className="renderer-components-error-message-wrapper">
+		<div
+			className={ classnames(
+				'renderer-components-error-message-wrapper',
+				css`
+					background: ${theme.errorsBgColor};
+					color: ${theme.errorsFontColor};
+				`
+			) }
+		>
 			{ message }
 		</div>
 	);
