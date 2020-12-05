51bbc6bc93f6cd7ac460c2cd8fee9192f959d91c
mdmag <md.magdy.sa@gmail.com>
2020-12-05 23:32:41 +0200

    Importing controls and resolvers

--- packages/theme-editor/src/store/index.js
+++ packages/theme-editor/src/store/index.js
@@ -1,10 +1,16 @@
 import { registerStore } from '@wordpress/data';
+import { controls as dataControls } from '@wordpress/data-controls';
+
 import reducer from './reducer';
 import * as actions from './actions';
 import * as selectors from './selectors';
+import * as resolvers from './resolvers';
+import * as controls from './controls';
 const store = registerStore( 'quillForms/theme-editor', {
 	reducer,
 	actions,
 	selectors,
+	resolvers,
+	controls: { ...dataControls, ...controls },
 } );
 export default store;
