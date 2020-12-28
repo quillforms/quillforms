9565fd4df1f119b20ee34debbabe6bc72452fb42
mdmag <md.magdy.sa@gmail.com>
2020-12-29 00:18:32 +0200

    Adding logic control for number block

--- packages/blocklib-number-block/src/admin/index.js
+++ packages/blocklib-number-block/src/admin/index.js
@@ -10,11 +10,13 @@ import { Event as DateIcon } from '@material-ui/icons';
  */
 import controls from './controls';
 import { type } from '../block.json';
+import logicControl from './logic-control';
 
 export const blockAdminSettings = {
 	color: '#93AE89',
 	icon: DateIcon,
 	controls,
+	logicControl,
 };
 
 setBlockAdminSettings( type, blockAdminSettings );
---
+++ packages/blocklib-number-block/src/admin/logic-control.js
@@ -0,0 +1,18 @@
+import { TextControl } from '@quillforms/builder-components';
+import { useEffect, useState } from '@wordpress/element';
+
+const NumberLogicControl = ( { value, setValue } ) => {
+	const [ controlVal, setControlVal ] = useState( 0 );
+	useEffect( () => {
+		setValue( parseInt( controlVal ) );
+	}, [ controlVal ] );
+	return (
+		<TextControl
+			type="number"
+			value={ value }
+			setValue={ ( val ) => setControlVal( val ) }
+		/>
+	);
+};
+
+export default NumberLogicControl;
