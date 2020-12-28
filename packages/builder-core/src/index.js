ca66e44bdd83bc16905cc621f0f5a01c55d1b671
mdmag <md.magdy.sa@gmail.com>
2020-12-29 00:46:05 +0200

    Removing import for block library package because it will be deleted

--- packages/builder-core/src/index.js
+++ packages/builder-core/src/index.js
@@ -1,12 +1,5 @@
-import '@wordpress/data';
-import '@wordpress/compose';
-import {
-	setBlocksRendererSettings,
-	setBlocksEditorSettings,
-} from '@quillforms/block-library';
+import '@quillforms/blocks';
 import '@quillforms/renderer-core';
 import '@quillforms/builder-panels';
 import './store';
 export * from './components';
-setBlocksRendererSettings();
-setBlocksEditorSettings();
