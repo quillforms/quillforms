56b72eec590d945a0a027880d0b63dc7c2b337fb
mdmag <md.magdy.sa@gmail.com>
2021-01-19 23:35:14 +0200

    Replacing the old concept for accessing metafields via hooks with having a general context for the form object

---
+++ packages/renderer-core/src/components/form-context/index.js
@@ -0,0 +1,30 @@
+/**
+ * WordPress Dependencies
+ */
+import React, { createContext, useMemo } from '@wordpress/element';
+import { noop } from '@quillforms/utils';
+
+const FormContext = createContext( {} );
+
+const FormContextProvider = ( { children, value } ) => {
+	const memoizedValue = useMemo( () => value, Object.values( value ) );
+
+	return (
+		<FormContext.Provider value={ memoizedValue }>
+			{ children }
+		</FormContext.Provider>
+	);
+};
+FormContextProvider.defaultProps = {
+	value: {
+		formObj: {
+			blocks: [],
+			messages: {},
+			theme: {},
+			logic: null,
+		},
+		onSubmit: noop,
+	},
+};
+
+export { FormContext, FormContextProvider };
