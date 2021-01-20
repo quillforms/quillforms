ea21c2c6049277be66c5acecbdea2091375bfda5
mdmag <md.magdy.sa@gmail.com>
2021-01-20 23:59:26 +0200

    Controlling renderer from preview component

--- packages/builder-core/src/components/preview-area-wrapper/index.js
+++ packages/builder-core/src/components/preview-area-wrapper/index.js
@@ -7,32 +7,47 @@ import { useEffect, useState } from '@wordpress/element';
  * QuillForms Dependencies
  */
 import { useGlobalEditorContext } from '@quillforms/builder-components';
-import { FormContentWrapper } from '@quillforms/renderer-core';
-import { useTheme } from '@quillforms/utils';
+import { Form } from '@quillforms/renderer-core';
+import { useTheme } from '@quillforms/theme-editor';
+
+/**
+ * WordPress Dependencies
+ */
 import { useSelect, useDispatch } from '@wordpress/data';
+
+/**
+ * External Dependencies
+ */
+import { cloneDeep } from 'lodash';
+
 /**
  * Internal Dependencies
  */
 import PreviewArea from '../preview-area';
 import { PreviewContextProvider } from '../preview-context';
 
+let $timer;
 const FormPreview = ( {} ) => {
 	const [ applyJumpLogic, setApplyJumpLogic ] = useState( false );
 	const [ selfDispatch, setSelfDispatch ] = useState( false );
 	const { fonts } = useGlobalEditorContext();
+	const theme = useTheme();
 	const { font } = useTheme();
-
-	const { currentBlockBeingEdited, blocks } = useSelect( ( select ) => {
-		return {
-			currentBlockBeingEdited: select(
-				'quillForms/block-editor'
-			).getCurrentBlockId(),
-			blocks: select( 'quillForms/block-editor' ).getBlocks(),
-		};
-	} );
+	const { currentBlockBeingEdited, blocks, messages } = useSelect(
+		( select ) => {
+			return {
+				currentBlockBeingEdited: select(
+					'quillForms/block-editor'
+				).getCurrentBlockId(),
+				blocks: select( 'quillForms/block-editor' ).getBlocks(),
+				messages: select( 'quillForms/messages-editor' ).getMessages(),
+			};
+		}
+	);
+	const { setSwiper } = useDispatch( 'quillForms/renderer-core' );
 
 	const { setCurrentBlock } = useDispatch( 'quillForms/block-editor' );
-
+	const { completeForm } = useDispatch( 'quillForms/renderer-core' );
 	const fontType = fonts[ font ];
 	let fontUrl;
 	switch ( fontType ) {
@@ -88,6 +103,79 @@ const FormPreview = ( {} ) => {
 		}
 	}, [ selfDispatch ] );
 
+	useEffect( () => {
+		clearTimeout( $timer );
+		if ( ! applyJumpLogic ) {
+			const formFields = blocks.filter(
+				( block ) =>
+					block.type !== 'thankyou-screen' &&
+					block.type !== 'welcome-screen'
+			);
+
+			const $thankyouScreens = blocks
+				.filter( ( block ) => block.type === 'thankyou-screen' )
+				.concat( {
+					id: 'default_thankyou_screen',
+					type: 'thankyou-screen',
+				} );
+
+			const $welcomeScreens = blocks.filter(
+				( block ) => block.type === 'welcome-screen'
+			);
+
+			const $isThankyouScreenActive =
+				blocks[ currentBlockBeingEditedIndex ]?.type ===
+				'thankyou-screen';
+
+			const $isWelcomeScreenActive =
+				blocks[ currentBlockBeingEditedIndex ]?.type ===
+				'welcome-screen';
+			const $currentPath = cloneDeep( formFields );
+			setSwiper( {
+				walkPath: $currentPath,
+				welcomeScreens: $welcomeScreens,
+				thankyouScreens: $thankyouScreens,
+				isThankyouScreenActive: $isThankyouScreenActive,
+				isWelcomeScreenActive: $isWelcomeScreenActive,
+			} );
+
+			const currentBlockBeingEditedIndex = blocks.findIndex(
+				( $block ) => $block.id === currentBlockBeingEdited
+			);
+
+			const currentFieldBeingEditedIndex = formFields.findIndex(
+				( $block ) => $block.id === currentBlockBeingEdited
+			);
+
+			const $nextBlockId =
+				! $isThankyouScreenActive &&
+				$currentPath?.length > 0 &&
+				$currentPath[ currentFieldBeingEditedIndex + 1 ]
+					? $currentPath[ currentFieldBeingEditedIndex + 1 ].id
+					: undefined;
+			const $prevBlockId =
+				! $isThankyouScreenActive &&
+				$currentPath?.length > 0 &&
+				$currentPath[ currentBlockBeingEditedIndex - 1 ]
+					? $currentPath[ currentBlockBeingEditedIndex - 1 ].id
+					: undefined;
+			$timer = setTimeout( () => {
+				setSwiper( {
+					isAnimating: true,
+					currentBlockId: currentBlockBeingEdited,
+					nextBlockId: $nextBlockId,
+					prevBlockId: $prevBlockId,
+					canGoNext: $isThankyouScreenActive ? false : true,
+					canGoPrev:
+						$prevBlockId && ! $isThankyouScreenActive
+							? true
+							: false,
+					isSubmissionScreenActive: undefined,
+				} );
+			}, 300 );
+		}
+	}, [ JSON.stringify( blocks ), currentBlockBeingEdited ] );
+
 	return (
 		<div className="builder-core-preview-area-wrapper">
 			<PreviewContextProvider
@@ -96,8 +184,16 @@ const FormPreview = ( {} ) => {
 				<PreviewArea.Slot>
 					{ ( fills ) => (
 						<>
-							<FormContentWrapper
-								applyJumpLogic={ applyJumpLogic }
+							<Form
+								formObj={ {
+									blocks,
+									theme,
+									messages,
+									logic: null,
+								} }
+								applyLogic={ applyJumpLogic }
+								onSubmit={ completeForm }
+								isPreview={ true }
 							/>
 							{ fills }
 						</>
