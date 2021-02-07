9424ac30f63b060741f47aa9304e223717041481
mdmag <md.magdy.sa@gmail.com>
2021-02-07 08:41:35 +0200

    Deleting utils package, no need for it

--- packages/utils/src/get-default-theme-properties.js
+++
@@ -1,6 +0,0 @@
-const getDefaultThemeProperties = () => {
-	return window?.qfEditorContext?.defaults?.theme?.properties
-		? window.qfEditorContext.defaults.theme.properties
-		: {};
-};
-export default getDefaultThemeProperties;
--- packages/utils/src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as isStoreRegistered } from './is-store-registered';
-export { default as getDefaultThemeProperties } from './get-default-theme-properties';
-export { default as useTheme } from './use-theme';
