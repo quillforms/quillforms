f0f88ddd31c6a53bd018681680f4af4c5f0a6cc2
mdmag <md.magdy.sa@gmail.com>
2021-02-07 08:44:21 +0200

    Using the new component for form card and having modifications in style

--- client/pages/home/index.js
+++ client/pages/home/index.js
@@ -8,19 +8,13 @@ import { Icon, plusCircle } from '@wordpress/icons';
 import { useEffect, useState } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 import { addQueryArgs } from '@wordpress/url';
-/**
- * External Dependencies
- */
-import { getNewPath } from '@woocommerce/navigation';
-import { Link } from 'react-router-dom';
-import { css } from 'emotion';
-import classnames from 'classnames';
 
 /**
  * Internal Dependencies
  */
 import './style.scss';
 import AddFormModal from './add-form-modal';
+import FormCard from './form-card';
 
 const Home = () => {
 	const [ isFetching, setIsFetching ] = useState( true );
@@ -40,7 +34,6 @@ const Home = () => {
 			method: 'GET',
 		} )
 			.then( ( res ) => {
-				console.log( res );
 				setPublishedForms( res );
 				setIsFetching( false );
 			} )
@@ -86,70 +79,7 @@ const Home = () => {
 					<Card className="quillforms-home__empty-form-card"></Card>
 				) : (
 					publishedForms.map( ( form ) => {
-						const theme = form.theme.theme_data;
-						return (
-							<Card
-								key={ form.id }
-								className="quillforms-home__form-card"
-							>
-								<Link
-									to={ getNewPath(
-										{},
-										`/forms/${ form.id }/builder`
-									) }
-									className={ css`
-										text-decoration: none;
-										display: flex;
-										flex-direction: column;
-										justify-content: center;
-										height: 100%;
-									` }
-								>
-									<CardBody
-										className={ classnames(
-											'quillforms-home__form-card-body',
-											css`
-												background-color: ${theme.backgroundColor};
-											`
-										) }
-									>
-										<div
-											className={ classnames(
-												'quillforms-home__form-title',
-												css`
-													color: ${theme.questionsColor};
-													text-decoration: none;
-												`
-											) }
-										>
-											{ form.title.rendered
-												? form.title.rendered
-												: 'Untitled' }
-										</div>
-									</CardBody>
-									<CardDivider />
-									<CardHeader
-										className={ classnames(
-											'quillforms-home__form-card-footer',
-											css`
-												background: ${form.status ===
-												'publish'
-													? '#18c485'
-													: '#8f8e8e'};
-												flex: 1 1;
-												border-radius: 0;
-											`
-										) }
-									>
-										<div className="quillforms-home__form-card-footer-post-status">
-											{ form.status === 'publish'
-												? 'Publish'
-												: 'Draft' }
-										</div>
-									</CardHeader>
-								</Link>
-							</Card>
-						);
+						return <FormCard key={ form.id } form={ form } />;
 					} )
 				) }
 			</div>
