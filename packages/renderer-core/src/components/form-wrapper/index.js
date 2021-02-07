fbf9d372e0eece13d8c6c4cefba1a9a1a5549dbb
mdmag <md.magdy.sa@gmail.com>
2021-02-07 07:09:31 +0200

    Adding form wrapper component

---
+++ packages/renderer-core/src/components/form-wrapper/index.js
@@ -0,0 +1,65 @@
+/**
+ * WordPress Dependencies
+ */
+import { useEffect } from '@wordpress/element';
+import { useDispatch } from '@wordpress/data';
+import { cloneDeep } from 'lodash';
+
+/**
+ * Internal Dependencies
+ */
+import useEditableFields from '../../hooks/use-editable-fields';
+import FormFlow from '../form-flow';
+import useBlocks from '../../hooks/use-blocks';
+
+const FormWrapper = ( { applyLogic, isPreview } ) => {
+	const editableFields = useEditableFields();
+	const blocks = useBlocks();
+	const { insertEmptyFieldAnswer } = useDispatch(
+		'quillForms/renderer-submission'
+	);
+	const { setSwiper } = useDispatch( 'quillForms/renderer-core' );
+	useEffect( () => {
+		if ( ! isPreview ) {
+			editableFields.forEach( ( field ) =>
+				insertEmptyFieldAnswer( field.id, field.type )
+			);
+			const firstBlock = blocks && blocks[ 0 ] ? blocks[ 0 ].id : null;
+			setTimeout( () => {
+				setSwiper( {
+					currentBlockId: firstBlock,
+					isAnimating: true,
+					walkPath: cloneDeep(
+						blocks.filter(
+							( block ) =>
+								block.type !== 'thankyou-screen' &&
+								block.type !== 'welcome-screen'
+						)
+					),
+					welcomeScreens: cloneDeep(
+						blocks.filter(
+							( block ) => block.type === 'welcome-screen'
+						)
+					),
+					thankyouScreens: cloneDeep(
+						blocks.filter(
+							( block ) => block.type === 'thankyou-screen'
+						)
+					),
+					isWelcomeScreenActive:
+						firstBlock?.type === 'welcome-screen' ? true : false,
+					isThankyouScreenActive:
+						firstBlock?.type === 'welcome-screen' ? true : false,
+					canGoPrev: false,
+					canGoNext: true,
+					prevBlockId: null,
+					nextBlockId: blocks && blocks[ 1 ] ? blocks[ 1 ].id : null,
+				} );
+			}, 0 );
+		}
+	}, [] );
+
+	return <FormFlow applyLogic={ applyLogic } />;
+};
+
+export default FormWrapper;
