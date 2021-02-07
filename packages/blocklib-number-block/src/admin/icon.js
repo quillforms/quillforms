f6237fa984626f16b45a57f7e3b76ac05660a252
mdmag <md.magdy.sa@gmail.com>
2021-02-07 08:31:57 +0200

    Adding admin icon for number block

---
+++ packages/blocklib-number-block/src/admin/icon.js
@@ -0,0 +1,15 @@
+const Icon = () => {
+	return (
+		<i
+			style={ {
+				verticalAlign: 'text-bottom',
+				fontWeight: 'bold',
+				color: '#fff',
+			} }
+		>
+			#
+		</i>
+	);
+};
+
+export default Icon;
