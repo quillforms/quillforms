803dfa5fb716abe0c5d2099cac8d99b21b495ab3
mdmag <md.magdy.sa@gmail.com>
2021-02-16 19:44:36 +0200

    Removing un-needed move focus to end function

--- packages/rich-text/src/move-focus-to-end.js
+++
@@ -1,9 +0,0 @@
-import focus from './focus';
-import { Transforms, Editor } from 'slate';
-
-const moveFocusToEnd = ( editor ) => {
-	focus( editor );
-	Transforms.select( editor, Editor.end( editor, [] ) );
-};
-
-export default moveFocusToEnd;
