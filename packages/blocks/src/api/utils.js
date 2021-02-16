3efb264d0286bbab478117d1b01ffc37da713ff3
mdmag <md.magdy.sa@gmail.com>
2021-02-16 19:10:32 +0200

    Adding normalizeIconObject function

--- packages/blocks/src/api/utils.js
+++ packages/blocks/src/api/utils.js
@@ -1,12 +1,19 @@
 /**
  * External dependencies
  */
-import { isFunction, isString } from 'lodash';
+import { has, isFunction, isString } from 'lodash';
 
 /**
  * WordPress dependencies
  */
 import { Component, isValidElement } from '@wordpress/element';
+
+/**
+ * External dependencies
+ */
+import { default as tinycolor, mostReadable } from 'tinycolor2';
+
+const ICON_COLORS = [ '#191e23', '#f8f9f9' ];
 
 /**
  * Function that checks if the parameter is a valid icon.
@@ -24,4 +31,39 @@ export function isValidIcon( icon ) {
 			isFunction( icon ) ||
 			icon instanceof Component )
 	);
+}
+
+/**
+ * Function that receives an icon as set by the blocks during the registration
+ * and returns a new icon object that is normalized so we can rely on just on possible icon structure
+ * in the codebase.
+ *
+ * @param {QFBlockTypeIconRender} icon Render behavior of a block type icon;
+ *                                     one of a Dashicon slug, an element, or a
+ *                                     component.
+ *
+ * @return {QFBlockTypeIconDescriptor} Object describing the icon.
+ */
+export function normalizeIconObject( icon ) {
+	if ( isValidIcon( icon ) ) {
+		return { src: icon };
+	}
+
+	if ( has( icon, [ 'background' ] ) ) {
+		const tinyBgColor = tinycolor( icon.background );
+
+		return {
+			...icon,
+			foreground: icon.foreground
+				? icon.foreground
+				: mostReadable( tinyBgColor, ICON_COLORS, {
+						includeFallbackColors: true,
+						level: 'AA',
+						size: 'large',
+				  } ).toHexString(),
+			shadowColor: tinyBgColor.setAlpha( 0.3 ).toRgbString(),
+		};
+	}
+
+	return icon;
 }
