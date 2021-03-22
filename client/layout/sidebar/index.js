a67e0e9046cfa113e96e4ccbb9db04359a4629aa
Mohamed Magdy <md.magdy.sa@gmail.com>
2021-03-22 07:09:15 +0200

    Render all submenu items as navlik and handling unknown routes

--- client/layout/sidebar/index.js
+++ client/layout/sidebar/index.js
@@ -1,28 +1,70 @@
 /**
  * External Dependencies
  */
-import { NavLink, withRouter, match } from 'react-router-dom';
+import { NavLink, withRouter, matchPath } from 'react-router-dom';
 
 /**
  * Internal Dependencies
  */
 import './style.scss';
-import { map } from 'lodash';
+import { forEach, map } from 'lodash';
+import { getPages } from '../controller';
 
+const clean = ( str ) => {
+	return str.replace( 'quillforms', '' ).replace( '&path=', '' );
+};
+
+const matchesRegesiteredRoutes = ( path ) => {
+	let ret = false;
+	forEach( getPages(), ( page ) => {
+		console.log( page.path );
+		console.log( path );
+		const match = matchPath( path, {
+			path: page.path,
+			exact: true,
+			strict: false,
+		} );
+		ret = true;
+		return;
+	} );
+	return ret;
+};
 const Sidebar = () => {
-	console.log( qfAdmin.submenuPages );
 	return (
 		<div className="qf-admin-sidebar">
 			<div className="qf-admin-sidebar-nav-items">
 				{ map( qfAdmin.submenuPages, ( page ) => {
-					return (
-						<NavLink
-							className="qf-admin-sidebar-nav-item"
-							to={ `?admin.php/page=${ page[ 2 ] }` }
-						>
-							{ page[ 0 ] }
-						</NavLink>
-					);
+					if (
+						matchesRegesiteredRoutes( '/' + clean( page[ 2 ] ) )
+					) {
+						return (
+							<NavLink
+								isActive={ ( match, location ) => {
+									if (
+										( location.pathname ===
+											clean( page[ 2 ] ) ) |
+										( location.pathname ===
+											'/' + clean( page[ 2 ] ) )
+									) {
+										return true;
+									}
+								} }
+								activeClassName="selected"
+								className="qf-admin-sidebar-nav-item"
+								to={ `admin.php?page=${ page[ 2 ] }` }
+							>
+								{ page[ 0 ] }
+							</NavLink>
+						);
+					} else {
+						return (
+							<a
+								href={ `admin.php?page=${ page[ 2 ] }` }
+								className="qf-admin-sidebar-nav-item"
+								target="_blank"
+							/>
+						);
+					}
 				} ) }
 			</div>
 		</div>
