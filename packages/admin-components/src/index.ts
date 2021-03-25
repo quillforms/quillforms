7e1db2b9bee1fa944ba661977598896d0329c505
Mohamed Magdy <md.magdy.sa@gmail.com>
2021-03-25 13:59:24 +0200

    Exporting the new added admin components and importing their styles

--- packages/admin-components/src/index.ts
+++ packages/admin-components/src/index.ts
@@ -10,6 +10,8 @@ export { default as __experimentalDragDropContext } from './drag-drop-context';
 export { default as __experimentalDraggable } from './draggable';
 export { default as __experimentalDroppable } from './droppable';
 export { default as FontPicker } from './font-picker';
+export { default as FormAdminBar } from './form-admin-bar';
+export { default as Logo } from './logo';
 export { default as ToggleControl } from './toggle-control';
 export { default as SelectControl } from './select-control';
 export { default as TextControl } from './text-control';
