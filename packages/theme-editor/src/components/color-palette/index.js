bc7565051dd35f56877eab24d57b325878a05e77
Mohamed Magdy <md.magdy.sa@gmail.com>
2021-04-25 11:10:33 +0200

    forking color picker and palette components from wordpress/components to add the alpha option

---
+++ packages/theme-editor/src/components/color-palette/index.js
@@ -0,0 +1,108 @@
+/**
+ * External dependencies
+ */
+import { map } from 'lodash';
+import tinycolor from 'tinycolor2';
+
+/**
+ * WordPress dependencies
+ */
+import { __, sprintf } from '@wordpress/i18n';
+import { useCallback, useMemo } from '@wordpress/element';
+import { ColorPicker } from '@wordpress/components';
+
+/**
+ * Internal Dependencies
+ */
+import CircularOptionPicker from '../circular-option-picker';
+
+export default function ColorPalette( {
+	clearable = true,
+	className,
+	colors,
+	disableCustomColors = false,
+	onChange,
+	value,
+} ) {
+	const clearColor = useCallback( () => onChange( undefined ), [ onChange ] );
+	const colorOptions = useMemo( () => {
+		return map( colors, ( { color, name } ) => {
+			console.log( color );
+			console.log( value );
+			return (
+				<CircularOptionPicker.Option
+					key={ color }
+					isSelected={ value === color }
+					selectedIconProps={
+						value === color
+							? {
+									fill: tinycolor
+										.mostReadable( color, [
+											'#000',
+											'#fff',
+										] )
+										.toRgbString(),
+							  }
+							: {}
+					}
+					tooltipText={
+						name ||
+						// translators: %s: color hex code e.g: "#f00".
+						sprintf( __( 'Color code: %s' ), color )
+					}
+					style={ { backgroundColor: color, color } }
+					onClick={
+						value === color ? clearColor : () => onChange( color )
+					}
+					aria-label={
+						name
+							? // translators: %s: The name of the color e.g: "vivid red".
+							  sprintf( __( 'Color: %s' ), name )
+							: // translators: %s: color hex code e.g: "#f00".
+							  sprintf( __( 'Color code: %s' ), color )
+					}
+				/>
+			);
+		} );
+	}, [ colors, value, onChange, clearColor ] );
+	const renderCustomColorPicker = () => (
+		<ColorPicker
+			color={ value }
+			onChangeComplete={ ( color ) => {
+				onChange( color.rgb );
+			} }
+			disableAlpha={ false }
+		/>
+	);
+
+	return (
+		<CircularOptionPicker
+			className={ className }
+			options={ colorOptions }
+			actions={
+				<>
+					{ ! disableCustomColors && (
+						<CircularOptionPicker.DropdownLinkAction
+							dropdownProps={ {
+								renderContent: renderCustomColorPicker,
+								contentClassName:
+									'components-color-palette__picker',
+							} }
+							buttonProps={ {
+								'aria-label': __( 'Custom color picker' ),
+							} }
+							linkText={ __( 'Custom color' ) }
+						/>
+					) }
+					{ !! clearable && (
+						<CircularOptionPicker.ButtonAction
+							onClick={ clearColor }
+						>
+							{ __( 'Clear' ) }
+						</CircularOptionPicker.ButtonAction>
+					) }
+				</>
+			}
+		/>
+	);
+}
--- packages/theme-editor/src/components/color-picker/index.tsx
+++ packages/theme-editor/src/components/color-picker/index.tsx
@@ -1,71 +1,111 @@
-import { ColorPalette as WPColorPicker } from '@wordpress/components';
+/**
+ * WordPress Dependencies
+ */
 import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal Dependencies
+ */
+import ColorPalette from '../color-palette';
 
 const ColorPicker = ( { value, onChange } ) => {
 	const colors = [
 		{
 			name: __( 'Pale pink' ),
 			slug: 'pale-pink',
-			color: '#f78da7',
+			color: 'rgba(247, 141, 167, 1)',
 		},
-		{ name: __( 'Vivid red' ), slug: 'vivid-red', color: '#cf2e2e' },
+		{
+			name: __( 'Vivid red' ),
+			slug: 'vivid-red',
+			color: 'rgba(207, 46, 46, 1)',
+		},
 		{
 			name: __( 'Luminous vivid orange' ),
 			slug: 'luminous-vivid-orange',
-			color: '#ff6900',
+			color: 'rgba(255, 105, 0, 1)',
 		},
 		{
 			name: __( 'Luminous vivid amber' ),
 			slug: 'luminous-vivid-amber',
-			color: '#fcb900',
+			color: 'rgba(252, 185, 0, 1)',
 		},
 		{
 			name: __( 'Light green cyan' ),
 			slug: 'light-green-cyan',
-			color: '#7bdcb5',
+			color: 'rgba(123, 220, 181, 1)',
 		},
 		{
 			name: __( 'Vivid green cyan' ),
 			slug: 'vivid-green-cyan',
-			color: '#00d084',
+			color: 'rgba(0, 208, 132, 1)',
 		},
 		{
 			name: __( 'Pale cyan blue' ),
 			slug: 'pale-cyan-blue',
-			color: '#8ed1fc',
+			color: 'rgba(142, 209, 252, 1)',
 		},
 		{
 			name: __( 'Vivid cyan blue' ),
 			slug: 'vivid-cyan-blue',
-			color: '#0693e3',
+			color: 'rgba(6, 147, 227, 1)',
 		},
 		{
 			name: __( 'Vivid purple' ),
 			slug: 'vivid-purple',
-			color: '#9b51e0',
+			color: 'rgba(155, 81, 224, 1)',
 		},
 		{
 			name: __( 'Dark Magenta' ),
 			slug: 'dark-magenta',
-			color: '#8b008b',
+			color: 'rgba(139, 0, 139, 1)',
 		},
 		{
 			name: __( 'Cyan bluish gray' ),
 			slug: 'cyan-bluish-gray',
-			color: '#abb8c3',
+			color: 'rgba(171, 184, 195, 1)',
 		},
 		{
 			name: __( 'Very dark gray' ),
 			slug: 'very-dark-gray',
-			color: '#313131',
+			color: 'rgba(49, 49, 49, 1)',
 		},
 	];
+	function hexToRgb( hex ) {
+		if ( /^#[0-9A-F]{6}$/i.test( hex ) ) {
+			var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(
+				hex
+			);
+			return {
+				r: parseInt( result[ 1 ], 16 ),
+				g: parseInt( result[ 2 ], 16 ),
+				b: parseInt( result[ 3 ], 16 ),
+			};
+		}
+		return hex;
+	}
 	return (
 		<div className="admin-components-color-picker">
-			<WPColorPicker
+			<ColorPalette
 				colors={ colors }
 				value={ value }
-				onChange={ ( val ) => onChange( val ) }
+				onChange={ ( val ) => {
+					console.log( val );
+					// If is hex
+					if (
+						typeof val === 'object' ||
+						/^#[0-9A-F]{6}$/i.test( val )
+					) {
+						const rgbVal = hexToRgb( val );
+
+						const rgbaValue = `rgba(${ rgbVal.r }, ${ rgbVal.g }, ${
+							rgbVal.b
+						}, ${ rgbVal.a !== undefined ? rgbVal.a : 1 })`;
+						onChange( rgbaValue );
+					} else {
+						onChange( val );
+					}
+				} }
 			/>
 		</div>
 	);
