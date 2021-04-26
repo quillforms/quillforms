530e73c55e93172ff59def6723ee1d4751b6c2a4
Mohamed Magdy <md.magdy.sa@gmail.com>
2021-04-26 08:32:46 +0200

    Having a function for getting default messages

---
+++ packages/utils/src/get-default-messages.ts
@@ -0,0 +1,27 @@
+/**
+ * QuillForms Dependencies
+ */
+import ConfigAPI, { FormMessages } from '@quillforms/config';
+
+/**
+ * External Dependencies
+ */
+import { reduce } from 'lodash';
+
+const getDefaultMessages = (): FormMessages => {
+	const messagesStructure = ConfigAPI.getMessagesStructure();
+	const res = reduce(
+		messagesStructure,
+		( accumulator, schema, key ) => {
+			if ( schema.hasOwnProperty( 'default' ) ) {
+				accumulator[ key ] = schema.default;
+			}
+
+			return accumulator;
+		},
+		{}
+	);
+	return res;
+};
+
+export default getDefaultMessages;
