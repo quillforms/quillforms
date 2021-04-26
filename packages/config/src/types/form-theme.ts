af5505582db6007d8d9ed6400c644e273225e9e0
Mohamed Magdy <md.magdy.sa@gmail.com>
2021-04-26 08:31:25 +0200

    Having the theme more strongly typed

--- packages/config/src/types/form-theme.ts
+++ packages/config/src/types/form-theme.ts
@@ -1,1 +1,13 @@
-export type FormTheme = Record< string, unknown >;
+export type FormTheme = {
+	font: string;
+	backgroundColor: string;
+	backgroundImage: string;
+	questionsColor: string;
+	answersColor: string;
+	buttonsFontColor: string;
+	buttonsBgColor: string;
+	errorsFontColor: string;
+	errorsBgColor: string;
+	progressBarFillColor: string;
+	progressBarBgColor: string;
+};
