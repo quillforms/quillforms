1582ba1350132fdec61a151585da620a7175a1f8
Mohamed Magdy <md.magdy.sa@gmail.com>
2021-04-26 06:16:34 +0200

    Exporting sanitizeBlocks and sanitizeBlockAttributes functions

--- packages/blocks/src/api/index.ts
+++ packages/blocks/src/api/index.ts
@@ -1,4 +1,4 @@
 export { setBlockAdminSettings } from './set-block-admin-settings';
 export { setBlockRendererSettings } from './set-block-renderer-settings';
 export { registerBlockType, getBlockType } from './registration';
-export { createBlock } from './factory';
+export { sanitizeBlocks, sanitizeBlockAttributes } from './utils';
