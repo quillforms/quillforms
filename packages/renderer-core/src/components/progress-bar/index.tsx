6cf1afda287ca9dc4a28bb15da215e5b86d8306b
Mohamed Magdy <md.magdy.sa@gmail.com>
2021-06-04 08:54:28 +0200

    Having progress bar label filled from messages

--- packages/renderer-core/src/components/progress-bar/index.tsx
+++ packages/renderer-core/src/components/progress-bar/index.tsx
@@ -7,17 +7,29 @@ import classnames from 'classnames';
 /**
  * Internal Dependencies
  */
-import useTheme from '../../hooks/use-theme';
+import { useTheme, useMessages } from '../../hooks';
 import useProgressPerecent from '../../hooks/use-progress-percent';
+import { HTMLParser } from '..';
 
 const ProgressBar = () => {
 	const theme = useTheme();
 	const percent = useProgressPerecent();
-
+	const messages = useMessages();
 	return (
 		<div className="renderer-core-progress-bar">
-			<div className="renderer-core-progress-bar__label">
-				{ percent }% completed
+			<div
+				className={ classnames(
+					'renderer-core-progress-bar__label',
+					css`
+						color: ${ theme.questionsColor };
+						font-size: 16px;
+						@media ( max-width: 600px ) {
+							font-size: 14px;
+						}
+					`
+				) }
+			>
+				<HTMLParser value={ messages[ 'label.progress.percent' ] } />
 			</div>
 			<div
 				className={ classnames(
