5ebb782ef43d9b425e0be76663a875790593df61
Mohamed Magdy <md.magdy.sa@gmail.com>
2021-06-12 12:34:33 +0200

    Removing un-needed hooks and use the new block api to enqueue the used blocks styles and scripts

--- includes/render/class-qf-form-renderer.php
+++ includes/render/class-qf-form-renderer.php
@@ -6,26 +6,6 @@ class QF_Form_Renderer {
 
 		add_action( 'init', array( __CLASS__, 'template_include' ) );
 
-		add_filter( 'show_admin_bar', '__return_false' );
-
-		// Remove all WordPress actions.
-		remove_all_actions( 'wp_head' );
-		remove_all_actions( 'wp_print_styles' );
-		remove_all_actions( 'wp_print_head_scripts' );
-		remove_all_actions( 'wp_footer' );
-
-		// Handle `wp_head`.
-		add_action( 'wp_head', 'wp_enqueue_scripts', 1 );
-		add_action( 'wp_head', 'wp_print_styles', 8 );
-		add_action( 'wp_head', 'wp_print_head_scripts', 9 );
-		add_action( 'wp_head', 'wp_site_icon' );
-
-		// Handle `wp_footer`.
-		add_action( 'wp_footer', 'wp_print_footer_scripts', 20 );
-
-		// Handle `wp_enqueue_scripts`.
-		remove_all_actions( 'wp_enqueue_scripts' );
-
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_assets' ), 9999999 );
 
 	}
@@ -71,13 +51,13 @@ class QF_Form_Renderer {
 			// Render styles for used blocks only.
 			foreach ( $blocks as $block ) {
 				$block_type         = QF_Blocks_Factory::get_instance()->get_registered( $block['name'] );
-				$wp_styles->queue[] = $block_type->block_styles['renderer'];
+				$wp_styles->queue[] = $block_type->block_renderer_assets['style'];
 			}
 
 			// Render scripts for used blocks only.
 			foreach ( $blocks as $block ) {
 				$block_type          = QF_Blocks_Factory::get_instance()->get_registered( $block['name'] );
-				$wp_scripts->queue[] = $block_type->block_scripts['renderer'];
+				$wp_scripts->queue[] = $block_type->block_renderer_assets['script'];
 			}
 		endif;
 	}
