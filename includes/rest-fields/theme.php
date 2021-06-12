9cb745ed74b05d7ed99de8b01f2dd10eb53f3d72
Mohamed Magdy <md.magdy.sa@gmail.com>
2021-06-12 12:12:59 +0200

    PHP linting

--- includes/rest-fields/theme.php
+++ includes/rest-fields/theme.php
@@ -15,10 +15,10 @@ register_rest_field(
 	array(
 		'get_callback'    => function( $object ) {
 			$form_id = $object['id'];
-			$theme_id =  (int) get_post_meta( $form_id, 'theme', true );
+			$theme_id = (int) get_post_meta( $form_id, 'theme', true );
 			return  array(
-				'id' => $theme_id,
-				'theme_data' => QF_Form_Theme_Model::get_theme($theme_id)
+				'id'         => $theme_id,
+				'theme_data' => QF_Form_Theme_Model::get_theme( $theme_id ),
 			);
 
 		},
@@ -26,7 +26,7 @@ register_rest_field(
 			$form_id = $object->ID;
 			// Calculation the previous value because update_post_meta returns false if the same value passed.
 			$prev_value = intval( get_post_meta( $form_id, 'theme', true ) );
-			if ( $prev_value == intval( $meta ) ) {
+			if ( intval( $meta ) === $prev_value ) {
 				return true;
 			}
 			$ret = update_post_meta( $form_id, 'theme', intval( $meta ) );
