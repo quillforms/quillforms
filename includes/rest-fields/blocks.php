ff92a30fdd22809ad76ee84c432e6936126deee6
Mohamed Magdy <md.magdy.sa@gmail.com>
2021-06-12 12:32:42 +0200

    Adding validation for attributes

--- includes/rest-fields/blocks.php
+++ includes/rest-fields/blocks.php
@@ -38,20 +38,20 @@ register_rest_field(
 		'get_callback'    => function( $object ) {
 			$form_id = $object['id'];
 
-			$value = get_post_meta( $form_id, 'blocks', true );
-			$value = $value ? $value : array();
+			$blocks = get_post_meta( $form_id, 'blocks', true );
+			$blocks = $blocks ? $blocks : array();
 
 			// Just to add missing attributes.
-			if ( ! empty( $value ) ) {
-				foreach ( $value as $index => $block ) {
+			if ( ! empty( $blocks ) ) {
+				foreach ( $blocks as $index => $block ) {
 					$block_type = QF_Blocks_Factory::get_instance()->create( $block );
 					if ( ! empty( $block_type ) ) {
 						$block_attributes              = $block['attributes'] ? $block['attributes'] : array();
-						$value[ $index ]['attributes'] = $block_type->prepare_attributes_for_render( $block_attributes );
+						$blocks[ $index ]['attributes'] = $block_type->prepare_attributes_for_render( $block_attributes );
 					}
 				}
 			}
-			return $value;
+			return $blocks;
 		},
 		'update_callback' => function( $meta, $object ) {
 			$form_id = $object->ID;
@@ -74,58 +74,48 @@ register_rest_field(
 		'schema'          => array(
 			'description' => __( 'Blocks', 'quillforms' ),
 			'arg_options' => array(
-				'sanitize_callback' => function( $value ) use ( $blocks_schema ) {
-					$value = rest_sanitize_value_from_schema(
-						$value,
+				'sanitize_callback' => function( $blocks ) use ( $blocks_schema ) {
+					$blocks = rest_sanitize_value_from_schema(
+						$blocks,
 						$blocks_schema
 					);
-					// if ( ! empty( $value ) ) {
-					// foreach ( $value as $index => $item ) {
-					// $block_type           = QF_Blocks_Factory::get_instance()->get_registered( $item['name'] );
-					// $value[ $index ]['attributes'] = rest_sanitize_value_from_schema(
-					// $item['attributes'] ? $item['attributes'] : array(),
-					// array(
-					// 'type'       => 'object',
-					// 'properties' => $block_type->get_attributes(),
-					// )
-					// );
-					// }
-					// }
-					return $value;
+					// @todo
+					// Add sanitization for block attributes.
+					return $blocks;
 				},
-				'validate_callback' => function ( $value ) use ( $blocks_schema ) {
+				'validate_callback' => function ( $blocks ) use ( $blocks_schema ) {
 					// Block validation except for attributes.
 					$validation = rest_validate_value_from_schema(
-						$value,
+						$blocks,
 						$blocks_schema
 					);
-					// if ( ! $validation instanceof WP_Error ) {
-					// if ( ! empty( $value ) ) {
-					// foreach ( $value as $index => $item ) {
-					// $block      = QF_Blocks_Factory::get_instance()->get_registered( $item['name'] );
-					// if($item['attributes']) {
-					// $validation = rest_validate_value_from_schema(
-					// $item['attributes'],
-					// array(
-					// 'type'       => 'object',
-					// 'properties' => $block->get_attributes(),
-					// )
-					// );
-
-					// If there is an error, get the error message and code then return new WP_Error with the index.
-					// if ( $validation instanceof WP_Error ) {
-					// $code    = $validation->get_error_code();
-					// $message = $validation->get_error_message();
-					// return new WP_Error( $code, '[' . $index . '] ' . $message );
-					// }
-					// }
-					// }
-					// }
-					// }
+					if ( ! is_wp_error( $validation ) ) {
+						if ( ! empty( $blocks ) ) {
+							foreach ( $blocks as $index => $block ) {
+								$block_type     = QF_Blocks_Factory::get_instance()->get_registered( $block['name'] );
+								if ( $block_type ) {
+									if ( $block['attributes'] ) {
+										$validation = rest_validate_value_from_schema(
+											$block['attributes'],
+											array(
+												'type' => 'object',
+												'properties' => $block_type->get_attributes_schema(),
+											)
+										);
+									}
+									// If there is an error, get the error message and code then return new WP_Error with the index.
+									if ( is_wp_error( $validation ) ) {
+										$code    = $validation->get_error_code();
+										$message = $validation->get_error_message();
+										return new WP_Error( $code, '[' . $index . '] ' . $message );
+									}
+								}
+							}
+						}
+					}
 					return $validation;
 				},
 			),
 		),
-
 	)
 );
