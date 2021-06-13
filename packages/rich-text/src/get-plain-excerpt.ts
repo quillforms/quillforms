ba01a22f7df356a90bba37cfd198591b7fe1431b
Mohamed Magdy <md.magdy.sa@gmail.com>
2021-06-13 08:51:58 +0200

    Let getPlainExcerpt remove \n as well

--- packages/rich-text/src/get-plain-excerpt.ts
+++ packages/rich-text/src/get-plain-excerpt.ts
@@ -2,7 +2,8 @@ const getPlainExcerpt = ( value?: string ): string => {
 	if ( ! value ) return '';
 	let $value = value
 		.replace( /(<([^>]+)>)/gi, '' )
-		.replace( /{{([a-zA-Z0-9]+):([a-zA-Z0-9-_]+)}}/g, '______' );
+		.replace( /{{([a-zA-Z0-9]+):([a-zA-Z0-9-_]+)}}/g, '______' )
+		.replace( /\n/g, ' ' );
 	if ( $value.length > 30 ) {
 		$value = $value.substr( 0, 30 );
 		$value = $value.substr( 0, $value.lastIndexOf( ' ' ) + 1 ) + '...';
