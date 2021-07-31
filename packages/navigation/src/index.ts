de2707f954127fe329a8d62ed47be240223d69fc
Mohamed Magdy <md.magdy.sa@gmail.com>
2021-07-31 18:46:57 +0200

    Exporting Redirect component from react router dom

--- packages/navigation/src/index.ts
+++ packages/navigation/src/index.ts
@@ -8,6 +8,7 @@ export {
 	matchPath,
 	BrowserRouter,
 	useParams,
+	Redirect,
 } from 'react-router-dom';
 export { default as NavigationPrompt } from 'react-router-navigation-prompt';
 export { getHistory, getNewPath } from '@woocommerce/navigation';
