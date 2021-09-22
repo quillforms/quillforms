a456dfddd90095461552d3f065e7f98629126461
Abdallah S. Hassan <abdallahhassan1024@gmail.com>
2021-09-22 16:24:09 +0200

    Implementing activate action at site store class

--- includes/site/class-store.php
+++ includes/site/class-store.php
@@ -8,6 +8,8 @@
 
 namespace QuillForms\Site;
 
+use Throwable;
+
 /**
  * Store Class
  *
@@ -48,6 +50,8 @@ class Store {
 	 */
 	private function __construct() {
 		$this->define_addons();
+
+		add_action( 'wp_ajax_quillforms_addon_activate', array( $this, 'ajax_activate' ) );
 	}
 
 	/**
@@ -74,6 +78,85 @@ class Store {
 			},
 			$this->addons
 		);
+	}
+
+	/**
+	 * Activate addon
+	 *
+	 * @param string $addon_slug Addon slug.
+	 * @return array
+	 */
+	public function activate( $addon_slug ) {
+		$plugin_file = $this->addons[ $addon_slug ]['plugin_file'];
+
+		if ( ! function_exists( 'activate_plugin' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		}
+
+		// try to activate.
+		try {
+			$result = activate_plugin( $plugin_file );
+			if ( is_wp_error( $result ) ) {
+				quillforms_get_logger()->error(
+					esc_html__( 'Unexpected output on activating the addon plugin', 'quillforms' ),
+					array(
+						'code'  => 'addon_activation_unexpected_output',
+						'error' => array(
+							'code'    => $result->get_error_code(),
+							'message' => $result->get_error_message(),
+							'data'    => $result->get_error_data(),
+						),
+					)
+				);
+			}
+		} catch ( Throwable $e ) {
+			quillforms_get_logger()->error(
+				esc_html__( 'Fatal error on activating the addon plugin', 'quillforms' ),
+				array(
+					'code'      => 'addon_activation_fatal_error',
+					'exception' => array(
+						'code'    => $e->getCode(),
+						'message' => $e->getMessage(),
+						'trace'   => $e->getTraceAsString(),
+					),
+				)
+			);
+		}
+
+		$is_activated = is_plugin_active( $plugin_file );
+		return array(
+			'success' => $is_activated,
+		);
+	}
+
+	/**
+	 * Ajax handler for activating addon
+	 *
+	 * @return mixed
+	 */
+	public function ajax_activate() {
+		$this->check_authorization();
+
+		// posted addon slug.
+		$addon_slug = trim( $_POST['addon'] ?? '' );
+		if ( empty( $addon_slug ) ) {
+			wp_send_json_error( esc_html__( 'Addon slug is required', 'quillforms' ), 400 );
+			exit;
+		}
+
+		// check addon existence.
+		if ( ! isset( $this->addons[ $addon_slug ] ) ) {
+			wp_send_json_error( esc_html__( 'Cannot find the addon', 'quillforms' ), 400 );
+			exit;
+		}
+
+		$result = $this->activate( $addon_slug );
+		if ( $result['success'] ) {
+			wp_send_json_success( esc_html__( 'Addon plugin activated successfully', 'quillforms' ), 200 );
+		} else {
+			wp_send_json_error( $result['message'] ?? esc_html__( 'Cannot activate the addon plugin, please check the log for details', 'quillforms' ), 422 );
+		}
+		exit;
 	}
 
 	/**
@@ -150,6 +233,26 @@ class Store {
 		}
 
 		$this->addons = $addons;
+	}
+
+	/**
+	 * Check ajax request authorization.
+	 * Sends error response and exit if not authorized.
+	 *
+	 * @return void
+	 */
+	private function check_authorization() {
+		// check for valid nonce field.
+		if ( ! check_ajax_referer( 'quillforms_site_store', '_nonce', false ) ) {
+			wp_send_json_error( esc_html__( 'Invalid nonce', 'quillforms' ), 403 );
+			exit;
+		}
+
+		// check for user capability.
+		if ( ! current_user_can( 'manage_quillforms' ) ) {
+			wp_send_json_error( esc_html__( 'Forbidden', 'quillforms' ), 403 );
+			exit;
+		}
 	}
 
 }
