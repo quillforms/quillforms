b864e64e45e66e351497f870a58ee7cf6ba5cca4
Abdallah S. Hassan <abdallahhassan1024@gmail.com>
2021-09-29 17:32:23 +0200

    Including provider connections and account by default at form rest fields

--- includes/addon/provider/rest/class-rest.php
+++ includes/addon/provider/rest/class-rest.php
@@ -38,7 +38,41 @@ abstract class REST extends Abstract_REST {
 		parent::__construct( $addon );
 
 		new static::$classes['account_controller']( $this->addon );
-		add_filter( 'rest_prepare_quill_forms', array( $this, 'add_form_data' ), 10, 3 ); // uncomment at subclass if needed.
+		add_filter( 'rest_prepare_quill_forms', array( $this, 'add_rest_data' ), 10, 3 );
+	}
+
+	/**
+	 * Add addon rest data to quill_forms post type
+	 *
+	 * @since 1.6.0
+	 *
+	 * @param WP_REST_Response $response The response object.
+	 * @param WP_Post          $post     Post object.
+	 * @param WP_REST_Request  $request  Request object.
+	 * @return WP_REST_Response
+	 */
+	public function add_rest_data( $response, $post, $request ) { // phpcs:ignore
+		$data = $response->get_data();
+
+		$data['addons'][ $this->addon->slug ] = $this->get_rest_data( $post );
+
+		$response->set_data( $data );
+		return $response;
+	}
+
+	/**
+	 * Get rest data
+	 *
+	 * @since 1.6.0
+	 *
+	 * @param WP_Post $post Post object.
+	 * @return mixed
+	 */
+	protected function get_rest_data( $post ) {
+		return array(
+			'connections' => $this->addon->form_data->get_filtered( $post->ID, 'connections' ) ?? array(),
+			'accounts'    => $this->addon->accounts->get_accounts(),
+		);
 	}
 
 }
