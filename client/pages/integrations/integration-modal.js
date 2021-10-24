edf250c2b1b770aa70b2baf556e1f169f72d13c8
Abdallah S. Hassan <abdallahhassan1024@gmail.com>
2021-10-24 07:51:52 +0200

    Improving integration modal

--- client/pages/integrations/integration-modal.js
+++ client/pages/integrations/integration-modal.js
@@ -9,16 +9,53 @@ import { Modal } from '@wordpress/components';
 import { css } from 'emotion';
 
 const IntegrationModal = ( { slug, integration, onClose } ) => {
-	return (
-		<Modal
-			title={ integration.title }
-			focusOnMount={ true }
-			onRequestClose={ onClose }
+	const icon = integration.icon;
+	const title = (
+		<div
 			className={ css`
-				width: 750px;
+				display: flex;
+				align-items: center;
+
+				svg,
+				img {
+					width: 40px;
+					height: 40px;
+					margin-right: 0.5rem;
+				}
 			` }
 		>
-			<integration.render slug={ slug } />
+			{ typeof icon === 'string' ? (
+				<img src={ icon } />
+			) : (
+				<IconComponent icon={ icon?.src ? icon.src : icon } />
+			) }{ ' ' }
+			{ integration.title }
+		</div>
+	);
+
+	return (
+		<Modal
+			title={ title }
+			focusOnMount={ true }
+			className={
+				`integration-modal integration-modal-${ slug } ` +
+				css`
+					width: 750px;
+
+					.components-modal__content {
+						padding: 0;
+
+						.components-modal__header {
+							margin: 0 0 45px;
+						}
+					}
+				`
+			}
+			onRequestClose={ onClose }
+			shouldCloseOnEsc={ false }
+			shouldCloseOnClickOutside={ false }
+		>
+			<integration.render slug={ slug } onClose={ onClose } />
 		</Modal>
 	);
 };
