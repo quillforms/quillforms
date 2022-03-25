6e5ac921fce681470917eaa00f1e442f53078816
Abdallah S. Hassan <abdallahhassan1024@gmail.com>
2022-03-26 00:46:36 +0200

    Adding mapping control with key control to admin components

---
+++ packages/admin-components/src/mapping-control/index.tsx
@@ -0,0 +1,47 @@
+/**
+ * WordPress Dependencies
+ */
+import { plus, closeSmall } from '@wordpress/icons';
+import { Icon } from '@wordpress/components';
+
+/**
+ * Internal Dependencies
+ */
+import {
+	default as MappingKeyControl,
+	MappingKeyControlProps,
+} from './key-control';
+
+interface Props {
+	keyProps: MappingKeyControlProps;
+	onAddClick?: () => void;
+	onRemoveClick?: () => void;
+}
+
+const MappingControl: React.FC< Props > = ( {
+	keyProps,
+	onAddClick,
+	onRemoveClick,
+} ) => {
+	return (
+		<div className="mapping-control">
+			<MappingKeyControl { ...keyProps } />
+			<div className="mapping-control-separator"></div>
+			<div className="mapping-control-buttons">
+				{ !! onRemoveClick && (
+					<div className="mapping-control-buttons-remove">
+						<Icon icon={ closeSmall } onClick={ onRemoveClick } />
+					</div>
+				) }
+				{ !! onAddClick && (
+					<div className="mapping-control-buttons-add">
+						<Icon icon={ plus } onClick={ onAddClick } />
+					</div>
+				) }
+			</div>
+		</div>
+	);
+};
+
+export { MappingControl };
+export { default as MappingKeyControl } from './key-control';
