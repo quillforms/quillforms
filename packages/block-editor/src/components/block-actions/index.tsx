9ca0f79be28456ede014a004351b44af8d49f77e
Mohamed Magdy <md.magdy.sa@gmail.com>
2022-04-25 06:41:13 +0200

    Adding duplicate feature for blocks

--- packages/block-editor/src/components/block-actions/index.tsx
+++ packages/block-editor/src/components/block-actions/index.tsx
@@ -1,7 +1,7 @@
 /**
  * WordPress Dependencies
  */
-import { useDispatch } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { doAction, applyFilters } from '@wordpress/hooks';
 import { DropdownMenu, MenuGroup, MenuItem, Icon } from '@wordpress/components';
 import { trash } from '@wordpress/icons';
@@ -10,18 +10,35 @@ import { trash } from '@wordpress/icons';
  * External Dependencies
  */
 import { confirmAlert } from 'react-confirm-alert';
+import classnames from 'classnames';
+import { css } from 'emotion';
 
 /**
  * Internal Dependencies
  */
 import DeleteAlert from '../delete-alert';
+import DuplicateIcon from '../duplicate-icon';
 
 interface Props {
 	id: string;
 }
 const BlockActions: React.FC< Props > = ( { id } ) => {
-	const { deleteBlock } = useDispatch( 'quillForms/block-editor' );
+	const { deleteBlock, __experimentalInsertBlock } = useDispatch(
+		'quillForms/block-editor'
+	);
 
+	const { block, currentBlockIndex } = useSelect( ( select ) => {
+		return {
+			block: select( 'quillForms/block-editor' ).getBlockById( id ),
+			currentBlockIndex: select(
+				'quillForms/block-editor'
+			).getCurrentBlockIndex(),
+		};
+	} );
+
+	if ( ! block ) {
+		return null;
+	}
 	// Delete Block
 	const handleDelete = ( e: React.MouseEvent ) => {
 		e.stopPropagation();
@@ -64,10 +81,38 @@ const BlockActions: React.FC< Props > = ( { id } ) => {
 			popoverProps={ {
 				position: 'bottom left',
 			} }
-			className="block-editor-block-actions__dropdown"
+			className={ classnames(
+				'block-editor-block-actions__dropdown',
+				css`
+					.components-menu-item__item {
+						min-width: auto;
+						svg {
+							margin: 0 5px;
+						}
+					}
+				`
+			) }
 		>
 			{ ( { onClose } ) => (
 				<MenuGroup className="block-editor-block-actions__menu-group">
+					<MenuItem
+						onClick={ (
+							e: React.MouseEvent< HTMLButtonElement >
+						) => {
+							onClose();
+							__experimentalInsertBlock(
+								{
+									...block,
+									id: Math.random()
+										.toString( 36 )
+										.substr( 2, 9 ),
+								},
+								{ index: currentBlockIndex + 1 }
+							);
+						} }
+					>
+						<DuplicateIcon /> Duplicate
+					</MenuItem>
 					<MenuItem
 						onClick={ (
 							e: React.MouseEvent< HTMLButtonElement >
