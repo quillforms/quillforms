0bbd4a973e8e77db0bd77e09ccfa5a601e042d05
Mohamed Magdy <md.magdy.sa@gmail.com>
2022-05-04 00:41:39 +0200

    Fix the order of imports

--- packages/react-renderer/src/index.ts
+++ packages/react-renderer/src/index.ts
@@ -1,9 +1,10 @@
 import '@quillforms/renderer-core/build-style/style.css';
-register( blocksStore );
-register( rendererStore );
-
 import { register } from '@wordpress/data';
 import { store as blocksStore } from '@quillforms/blocks';
 import { store as rendererStore } from '@quillforms/renderer-core';
+
+register( blocksStore );
+register( rendererStore );
+
 export { default as registerCoreBlocks } from './register-blocks';
 export { Form } from '@quillforms/renderer-core';
