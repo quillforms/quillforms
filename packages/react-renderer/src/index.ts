0f21709997f4f76f45ad4c6639f105312e3e64aa
Mohamed Magdy <md.magdy.sa@gmail.com>
2022-05-03 22:54:02 +0200

    Registering stores in react renderer package

--- packages/react-renderer/src/index.ts
+++ packages/react-renderer/src/index.ts
@@ -1,3 +1,11 @@
 import '@quillforms/renderer-core/build-style/style.css';
+import { register } from '@wordpress/data';
+import { store as blocksStore } from '@quillforms/blocks';
+import { store as rendererStore } from '@quillforms/renderer-core';
 export { default as registerCoreBlocks } from './register-blocks';
 export { Form } from '@quillforms/renderer-core';
+
+export const registerStores = () => {
+	register( blocksStore );
+	register( rendererStore );
+};
