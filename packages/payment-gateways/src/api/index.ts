f3c15cafebc2da3e923d36a8a2bce35665cbee1a
Mohamed Magdy <md.magdy.sa@gmail.com>
2022-08-07 19:05:50 +0200

    removing requirements of settings

--- packages/payment-gateways/src/api/index.ts
+++ packages/payment-gateways/src/api/index.ts
@@ -59,11 +59,6 @@ export const registerPaymentGatewayModule = (
 		return;
 	}
 
-	// if ( ! module.settings ) {
-	// 	console.error( `The 'settings' property is mandatory!` );
-	// 	return;
-	// }
-
 	if ( module.settings && ! isFunction( module.settings ) ) {
 		console.error( 'The "settings" property must be a valid function!' );
 		return;
