6de3d4dec558d4280b9667f167e77931bad0a25e
Mohamed Magdy <md.magdy.sa@gmail.com>
2023-02-24 02:02:37 +0200

    Updating version

--- quillforms.php
+++ quillforms.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Quill Forms
  * Plugin URI:        https://www.quillforms.com/
  * Description:       Conversational Forms Builder for WordPress
- * Version:           2.3.0
+ * Version:           2.4.0
  * Author:            quillforms.com
  * Author URI:        http://www.quillforms.com
  * Text Domain:       quillforms
@@ -22,7 +22,7 @@ if (! defined('QUILLFORMS_PLUGIN_FILE') ) {
 
 // Plugin version.
 if (! defined('QUILLFORMS_VERSION') ) {
-    define('QUILLFORMS_VERSION', '2.3.0');
+    define('QUILLFORMS_VERSION', '2.4.0');
 }
 
 // Plugin Folder Path.
