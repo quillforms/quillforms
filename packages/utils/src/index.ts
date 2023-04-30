dd1ec61d802c7767a7394758e711156812434929
Mohamed Magdy <md.magdy.sa@gmail.com>
2023-04-30 20:04:25 +0300

    Remove useTrace from being exported in utils package

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,3 @@
-export { default as useTrace } from './use-trace-updates';
 export { default as getDefaultThemeProperties } from './get-default-theme-properties';
 export { default as getDefaultMessages } from './get-default-messages';
 export { default as identAlphabetically } from './ident-alphabetically';
