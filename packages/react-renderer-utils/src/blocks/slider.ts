7929e811d74a1f331c3532a5e7d39db12cd40c22
mdmag <md.magdy.sa@gmail.com>
2023-07-04 01:46:23 +0300

    Adding slider block in react renderer utils package

---
+++ packages/react-renderer-utils/src/blocks/slider.ts
@@ -0,0 +1,11 @@
+import { registerBlockType } from '@quillforms/blocks';
+import { name, metadata } from '@quillforms/blocklib-slider-block';
+// eslint-disable-next-line no-restricted-syntax
+import rendererSettings from '@quillforms/blocklib-slider-block/build/renderer/settings';
+const register = () => {
+	registerBlockType( name, {
+		...metadata,
+		...rendererSettings,
+	} );
+};
+export default register;
