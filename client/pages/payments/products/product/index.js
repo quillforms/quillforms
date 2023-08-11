a81b308017694f6a50b321f820c0c3278496ee25
mdmag <md.magdy.sa@gmail.com>
2023-08-11 23:05:59 +0300

    Fixing getting variables to make sure resolvers work

--- client/pages/payments/products/product/index.js
+++ client/pages/payments/products/product/index.js
@@ -13,57 +13,58 @@ import NumericProduct from './numeric-product';
 import ChoicesProduct from './choices-product';
 import UnspecifiedProduct from './unspecified-product';
 
-const Product = ( { id } ) => {
+const Product = ({ id }) => {
 	const { products, errors } = usePaymentsContext();
-	const product = products[ id ];
+	const product = products[id];
 
-	const { blocks, blockTypes } = useSelect( ( select ) => {
+	const { blocks, blockTypes, variables } = useSelect((select) => {
 		return {
-			blocks: select( 'quillForms/block-editor' ).getBlocks() ?? [],
-			blockTypes: select( 'quillForms/blocks' ).getBlockTypes() ?? {},
+			blocks: select('quillForms/block-editor').getBlocks() ?? [],
+			blockTypes: select('quillForms/blocks').getBlockTypes() ?? {},
+			variables: select('quillForms/logic-editor')?.getLogic()?.variables ?? [],
 		};
-	} );
+	});
 
 	let element;
 
-	if ( product.source ) {
-		switch ( product.source.type ) {
+	if (product.source) {
+		switch (product.source.type) {
 			case 'field':
 				const block = blocks.find(
-					( block ) => block.id === product.source.value
+					(block) => block.id === product.source.value
 				);
-				if ( block ) {
-					const blockType = blockTypes[ block.name ];
-					if ( blockType.supports.numeric ) {
-						element = <NumericProduct id={ id } />;
-					} else if ( blockType.supports.choices ) {
-						element = <ChoicesProduct id={ id } />;
+				if (block) {
+					const blockType = blockTypes[block.name];
+					if (blockType.supports.numeric) {
+						element = <NumericProduct id={id} />;
+					} else if (blockType.supports.choices) {
+						element = <ChoicesProduct id={id} />;
 					}
 				}
 				break;
 			case 'variable':
-				element = <NumericProduct id={ id } />;
+				element = <NumericProduct id={id} />;
 				break;
 			case 'other':
-				if ( product.source.value === 'defined' ) {
-					element = <DefinedProduct id={ id } />;
+				if (product.source.value === 'defined') {
+					element = <DefinedProduct id={id} />;
 				}
 				break;
 		}
 	}
 
-	if ( ! element ) {
-		element = <UnspecifiedProduct id={ id } />;
+	if (!element) {
+		element = <UnspecifiedProduct id={id} />;
 	}
 
 	return (
 		<div className="quillforms-payments-page-settings-product">
-			{ element }
-			{ errors.products[ id ] && (
+			{element}
+			{errors.products[id] && (
 				<div className="quillforms-payments-page-settings-product-error">
-					{ errors.products[ id ] }
+					{errors.products[id]}
 				</div>
-			) }
+			)}
 		</div>
 	);
 };
