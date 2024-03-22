25b27e533d9b4682953a804085a950fe6c5078ed
aaharidy <abdoharidy72@gmail.com>
2024-03-23 00:48:36 +0200

    funnelkit, groundhogg

--- includes/site/class-store.php
+++ includes/site/class-store.php
@@ -1112,10 +1112,30 @@ class Store {
 					'name'           => esc_html__( 'GoHighLevel', 'quillforms' ),
 					'description'    => esc_html__( 'GoHighLevel addon allows you to connect your forms with GoHighLevel to send your leads to your GoHighLevel account.', 'quillforms' ),
 					'plugin_file'    => 'quillforms-gohighlevel/quillforms-gohighlevel.php',
-					'plan'           => 'basic',
+					'plan'           => 'plus',
 					'is_integration' => true,
 					'assets'         => array(
 						'icon' => QUILLFORMS_PLUGIN_URL . 'assets/addons/gohighlevel/icon.png',
+					),
+				),
+				'funnelkit'                 => array(
+					'name'           => esc_html__( 'FunnelKit', 'quillforms' ),
+					'description'    => esc_html__( 'FunnelKit addon allows you to connect your forms with FunnelKit Automations.', 'quillforms' ),
+					'plugin_file'    => 'quillforms-funnelkit/quillforms-funnelkit.php',
+					'plan'           => 'plus',
+					'is_integration' => true,
+					'assets'         => array(
+						'icon' => QUILLFORMS_PLUGIN_URL . 'assets/addons/funnelkit/icon.svg',
+					),
+				),
+				'groundhogg'                => array(
+					'name'           => esc_html__( 'Groundhogg', 'quillforms' ),
+					'description'    => esc_html__( 'Groundhogg addon allows you to connect your forms with Groundhogg to send your leads to your Groundhogg.', 'quillforms' ),
+					'plugin_file'    => 'quillforms-groundhogg/quillforms-groundhogg.php',
+					'plan'           => 'plus',
+					'is_integration' => true,
+					'assets'         => array(
+						'icon' => QUILLFORMS_PLUGIN_URL . 'assets/addons/groundhogg/icon.svg',
 					),
 				),
 				'wpuserregistration'        => array(
