5db4e62af29ac450c4889b01b21b3794dc83f70a
mdmag <md.magdy.sa@gmail.com>
2024-12-24 15:33:19 +0200

    Fixing security issue with Shortcode attributes

--- includes/class-shortcode.php
+++ includes/class-shortcode.php
@@ -30,7 +30,7 @@ class Shortcode
      */
     public static function instance()
     {
-        if (! self::$instance ) {
+        if (!self::$instance) {
             self::$instance = new self();
         }
         return self::$instance;
@@ -43,7 +43,7 @@ class Shortcode
      */
     private function __construct()
     {
-        add_action('init', array( $this, 'register' ));
+        add_action('init', array($this, 'register'));
     }
 
     /**
@@ -55,8 +55,8 @@ class Shortcode
      */
     public function register()
     {
-        add_shortcode('quillforms', array( $this, 'handler' ));
-        add_shortcode('quillforms-popup', array( $this, 'popup_handler' ));
+        add_shortcode('quillforms', array($this, 'handler'));
+        add_shortcode('quillforms-popup', array($this, 'popup_handler'));
     }
 
     /**
@@ -67,54 +67,64 @@ class Shortcode
      * @param  array $atts Shortcode attributes.
      * @return string
      */
-    public function handler( $atts )
+    public function handler($atts)
     {
         $atts = shortcode_atts(
             array(
-            'id'     => null,
-            'width'  => '100%',
-            'min_height' => null,
-            'max_height' => null,
-            'height' => null,
+                'id'         => null,
+                'width'      => '100%',
+                'min_height' => null,
+                'max_height' => null,
+                'height'     => null,
             ),
             $atts,
             'quillforms'
         );
 
-        $id     = (int) $atts['id'];
-        $width  = isset($atts['width']) ? esc_attr($atts['width']) : '100%';
-        $height = isset($atts['height']) ? esc_attr($atts['height']) : null;
-        $min_height = isset($atts['min_height']) ? esc_attr($atts['min_height']) : null;
-        $max_height = isset($atts['max_height']) ? esc_attr($atts['max_height']) : null;
-        if(!$min_height && $height) {
+        $id = (int) $atts['id'];
+        $width = esc_attr($atts['width']);
+        $height = esc_attr($atts['height']);
+        $min_height = esc_attr($atts['min_height']);
+        $max_height = esc_attr($atts['max_height']);
+
+        if (!$min_height && $height) {
             $min_height = $height;
         }
         if (!$min_height) {
             $min_height = "500px";
         }
-        if(!$max_height && $height) {
+        if (!$max_height && $height) {
             $max_height = $height;
         }
-        if(!$max_height) {
+        if (!$max_height) {
             $max_height = 'auto';
         }
-        if ('quill_forms' !== get_post_type($id) ) {
-            return 'Invalid form id';
+        if ('quill_forms' !== get_post_type($id)) {
+            return esc_html__('Invalid form ID', 'quillforms');
         }
 
         $src = get_permalink($id);
-        $src = add_query_arg(
-            array(
-            'quillforms-shortcode'   => true,
-            'quillforms-redirection' => 'top', // @deprecated 1.11.1
-            ),
-            $src
+        $src = esc_url(
+            add_query_arg(
+                array(
+                    'quillforms-shortcode'   => true,
+                    'quillforms-redirection' => 'top', // @deprecated 1.11.1
+                ),
+                $src
+            )
         );
+
         // wp_enqueue_script('quillforms-iframe-resizer');
         wp_enqueue_script('quillforms-iframe-resizer-implementer');
 
-        return "<iframe data-max-height='$max_height' class='quillforms-iframe' scrolling='no' src='$src' width='$width' style='border:0;min-height:$min_height; max-height: $max_height'></iframe>";
-
+        return sprintf(
+            "<iframe data-max-height='%s' class='quillforms-iframe' scrolling='no' src='%s' width='%s' style='border:0;min-height:%s;max-height:%s'></iframe>",
+            esc_attr($max_height),
+            $src,
+            esc_attr($width),
+            esc_attr($min_height),
+            esc_attr($max_height)
+        );
     }
 
     /**
@@ -125,110 +135,128 @@ class Shortcode
      * @param  array $atts Shortcode attributes.
      * @return string
      */
-    public function popup_handler( $atts ) {
+    public function popup_handler($atts)
+    {
         $atts = shortcode_atts(
             array(
-                'id' => null,
-                'buttontitle' => 'Open Form',
+                'id'                   => null,
+                'buttontitle'          => 'Open Form',
                 'buttonbackgroundcolor' => '#000000',
-                'buttontextColor' => '#ffffff',
-                'buttonborderradius' => '24',
-                'buttonborderwidth' => '0',
-                'buttonbordercolor' => '#000000',
-                'buttonfontsize' => '16',
-                'buttonpadding' => '10px 20px',
-                'popupmaxwidth' => '90',
-                'popupmaxwidthunit' => '%',
-                'popupmaxheight' => '90',
-                'popupmaxheightunit' => '%',
+                'buttontextcolor'      => '#ffffff',
+                'buttonborderradius'   => '24',
+                'buttonborderwidth'    => '0',
+                'buttonbordercolor'    => '#000000',
+                'buttonfontsize'       => '16',
+                'buttonpadding'        => '10px 20px',
+                'popupmaxwidth'        => '90',
+                'popupmaxwidthunit'    => '%',
+                'popupmaxheight'       => '90',
+                'popupmaxheightunit'   => '%',
             ),
             $atts,
             'quillforms-popup'
         );
 
-        
-        $id     = (int) $atts['id'];
-        $buttonTitle = isset($atts['buttontitle']) ? $atts['buttontitle'] : 'Open Form';
-        $buttonBackgroundColor = isset($atts['buttonbackgroundcolor']) ? $atts['buttonbackgroundcolor'] : '#000000';
-        $buttonTextColor = isset($atts['buttontextcolor']) ? $atts['buttontextcolor'] : '#ffffff';
-        $buttonBorderRadius = isset($atts['buttonborderradius']) ? $atts['buttonborderradius'] : '24';
-        $buttonBorderWidth = isset($atts['buttonborderwidth']) ? $atts['buttonborderwidth'] : '0';
-        $buttonBorderColor = isset($atts['buttonbordercolor']) ? $atts['buttonbordercolor'] : '#000000';
-        $buttonFontSize = isset($atts['buttonfontsize']) ? $atts['buttonfontsize'] : '16';
-        $buttonPadding = isset($atts['buttonpadding']) ? $atts['buttonpadding'] : '10px 20px';
-        // popupMaxWidth: '90',
-        //popUpMaxWidthUnit: '%',
-        //popuoMaxHeight: '90',
-        // popUpMaxHeightUnit: '%',
-        $popupMaxWidth = isset($atts['popupmaxwidth']) ? $atts['popupmaxwidth'] : '90';
-        $popupMaxWidthUnit = isset($atts['popupmaxwidthunit']) ? $atts['popupmaxwidthunit'] : '%';
-        $popupMaxHeight = isset($atts['popupmaxheight']) ? $atts['popupmaxheight'] : '90';
-        $popupMaxHeightUnit = isset($atts['popupmaxheightunit']) ? $atts['popupmaxheightunit'] : '%';
-
-        $permalink = get_permalink($id);
-        if ('quill_forms' !== get_post_type($id) ) {
-            return 'Invalid form id';
+        $id = (int) $atts['id'];
+        if ('quill_forms' !== get_post_type($id)) {
+            return esc_html__('Invalid form ID', 'quillforms');
         }
 
-        $src = add_query_arg(
-            array(
-                'quillforms-shortcode'   => true,
-                'quillforms-redirection' => 'top', // @deprecated 1.11.1
-            ),
-            $permalink
+        $buttonTitle = esc_html($atts['buttontitle']);
+        $buttonBackgroundColor = esc_attr($atts['buttonbackgroundcolor']);
+        $buttonTextColor = esc_attr($atts['buttontextcolor']);
+        $buttonBorderRadius = (int) $atts['buttonborderradius'];
+        $buttonBorderWidth = (int) $atts['buttonborderwidth'];
+        $buttonBorderColor = esc_attr($atts['buttonbordercolor']);
+        $buttonFontSize = (int) $atts['buttonfontsize'];
+        $buttonPadding = esc_attr($atts['buttonpadding']);
+        $popupMaxWidth = esc_attr($atts['popupmaxwidth']);
+        $popupMaxWidthUnit = esc_attr($atts['popupmaxwidthunit']);
+        $popupMaxHeight = esc_attr($atts['popupmaxheight']);
+        $popupMaxHeightUnit = esc_attr($atts['popupmaxheightunit']);
+
+        $src = esc_url(
+            add_query_arg(
+                array(
+                    'quillforms-shortcode'   => true,
+                    'quillforms-redirection' => 'top', // @deprecated 1.11.1
+                ),
+                get_permalink($id)
+            )
         );
 
         wp_enqueue_script('quillforms-popup');
         wp_enqueue_style('quillforms-popup-style');
-        return '<div class="quillforms-popup-button-wrapper">
-            <a class="quillforms-popup-button" style="
-                background-color: ' . $buttonBackgroundColor . ';
-                color: ' . $buttonTextColor . ';
-                border-radius: ' . $buttonBorderRadius . 'px;
-                border-width: ' . $buttonBorderWidth . 'px;
-                border-color: ' . $buttonBorderColor . ';
-                font-size: ' . $buttonFontSize . 'px;
-                padding: ' . $buttonPadding . ';
-                text-decoration: none;
-                cursor:pointer;
-            "
-                data-url="' . $src . '"
-                data-formId="' . $id . '"
-            >
-                ' . $buttonTitle . '
-            </a>
-            <div class="quillforms-popup-overlay" style="
-                position: fixed;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                background-color: rgba( 0, 0, 0, 0.8 );
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                z-index: -1;
-                opacity: 0;
-                transition: opacity 0.3s ease-in-out;
-                pointer-events: none;
-                visibility: hidden;
-            " data-formId="' . $id . '">
-                <div class="quillforms-popup-container" style="
-                    max-width: ' . $popupMaxWidth . $popupMaxWidthUnit . ';
-                    max-height: ' . $popupMaxHeight . $popupMaxHeightUnit . ';
+
+        return sprintf(
+            '<div class="quillforms-popup-button-wrapper">
+                <a class="quillforms-popup-button" style="
+                    background-color: %s;
+                    color: %s;
+                    border-radius: %dpx;
+                    border-width: %dpx;
+                    border-color: %s;
+                    font-size: %dpx;
+                    padding: %s;
+                    text-decoration: none;
+                    cursor: pointer;
+                "
+                data-url="%s"
+                data-formId="%d"
+                >
+                    %s
+                </a>
+                <div class="quillforms-popup-overlay" style="
+                    position: fixed;
+                    top: 0;
+                    left: 0;
                     width: 100%;
                     height: 100%;
-                ">
-                    <div class="quillforms-popup-close">
-                        <svg fill="currentColor" height="32" width="32" viewBox="0 0 24 24" style="display: inline-block; vertical-align: middle;"><path d="M0 0h24v24H0z" fill="none"></path><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
-                    </div>
-                    <div class="quillforms-popup-iframe-wrapper">
-                        <iframe data-no-lazy="true" src="' . $src . '" width="100%" height="100%" style="border:0;max-height: auto !important; max-width: auto !important;"></iframe>
-                        <div class="quillforms-popup-loader"><div class="quillforms-loading-circle"></div></div>
+                    background-color: rgba(0, 0, 0, 0.8);
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    z-index: -1;
+                    opacity: 0;
+                    transition: opacity 0.3s ease-in-out;
+                    pointer-events: none;
+                    visibility: hidden;
+                " data-formId="%d">
+                    <div class="quillforms-popup-container" style="
+                        max-width: %s%s;
+                        max-height: %s%s;
+                        width: 100%;
+                        height: 100%;
+                    ">
+                        <div class="quillforms-popup-close">
+                            <svg fill="currentColor" height="32" width="32" viewBox="0 0 24 24" style="display: inline-block; vertical-align: middle;">
+                                <path d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+                            </svg>
+                        </div>
+                        <div class="quillforms-popup-iframe-wrapper">
+                            <iframe data-no-lazy="true" src="%s" width="100%%" height="100%%" style="border:0;max-height:auto !important; max-width:auto !important;"></iframe>
+                            <div class="quillforms-popup-loader"><div class="quillforms-loading-circle"></div></div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            </div>';
+            </div>',
+            $buttonBackgroundColor,
+            $buttonTextColor,
+            $buttonBorderRadius,
+            $buttonBorderWidth,
+            $buttonBorderColor,
+            $buttonFontSize,
+            $buttonPadding,
+            $src,
+            $id,
+            $buttonTitle,
+            $id,
+            $popupMaxWidth,
+            $popupMaxWidthUnit,
+            $popupMaxHeight,
+            $popupMaxHeightUnit,
+            $src
+        );
     }
-
 }
