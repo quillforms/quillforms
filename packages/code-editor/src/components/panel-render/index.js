a26cc9065441d7fb9bbccd2a1df9a678e9612017
mdmag <md.magdy.sa@gmail.com>
2024-12-29 07:26:51 +0200

    Adding info for css that it will take effect in preview only

--- packages/code-editor/src/components/panel-render/index.js
+++ packages/code-editor/src/components/panel-render/index.js
@@ -13,42 +13,56 @@ import 'ace-builds/src-noconflict/theme-solarized_dark';
 import 'ace-builds/src-noconflict/ext-language_tools';
 
 const PanelRender = () => {
-	const { customCSS } = useSelect( ( select ) => {
+	const { customCSS } = useSelect((select) => {
 		return {
-			customCSS: select( 'quillForms/code-editor' ).getCustomCSS(),
+			customCSS: select('quillForms/code-editor').getCustomCSS(),
 		};
-	} );
+	});
 
-	const { setCustomCSS } = useDispatch( 'quillForms/code-editor' );
+	const { setCustomCSS } = useDispatch('quillForms/code-editor');
 	return (
 		<div
 			id="code-editor-panel-render"
-			className={ css`
+			className={css`
 				height: 100%;
-				> div {
-					width: 100% !important;
-					height: 100% !important;
-				}
 			` }
 		>
-			<AceEditor
-				mode="css"
-				theme="solarized_dark"
-				value={ customCSS }
-				onChange={ setCustomCSS }
-				name="QUILLFORMS_CSS_EDITOR"
-				showPrintMargin={ true }
-				showGutter={ true }
-				highlightActiveLine={ true }
-				editorProps={ { $blockScrolling: true } }
-				setOptions={ {
-					enableBasicAutocompletion: true,
-					enableLiveAutocompletion: true,
-					enableSnippets: true,
-					showLineNumbers: true,
-					tabSize: 2,
-				} }
-			/>
+			<div className={css`
+				padding: 10px;
+				background: antiquewhite;
+				margin-bottom: 10px;
+				height: 50px;
+				border-radius: 5px;
+				`}>
+				Custom css won't be applied to the form builder directly. Please click on the preview icon on the top right corner to see the changes.
+			</div>
+			<div className={css`
+				height: calc(100% - 60px);
+				width: 100%;
+				> div {
+					width: 100%;
+					height: 100%;
+				}
+			`}>
+				<AceEditor
+					mode="css"
+					theme="solarized_dark"
+					value={customCSS}
+					onChange={setCustomCSS}
+					name="QUILLFORMS_CSS_EDITOR"
+					showPrintMargin={true}
+					showGutter={true}
+					highlightActiveLine={true}
+					editorProps={{ $blockScrolling: true }}
+					setOptions={{
+						enableBasicAutocompletion: true,
+						enableLiveAutocompletion: true,
+						enableSnippets: true,
+						showLineNumbers: true,
+						tabSize: 2,
+					}}
+				/>
+			</div>
 			,
 		</div>
 	);
