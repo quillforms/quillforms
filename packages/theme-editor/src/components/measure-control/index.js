3615a0b34483f35eae3330b76ad7effff3df122f
emy185 <emyy729@gmail.com>
2026-04-08 14:54:13 +0200

    Add color customization to MeasureControl component

--- packages/theme-editor/src/components/measure-control/index.js
+++ packages/theme-editor/src/components/measure-control/index.js
@@ -2,6 +2,9 @@ import { RangeControl } from '@wordpress/components';
 const MeasureControl = ( { val, onChange } ) => {
 	return (
 		<RangeControl
+			color="#b2328c"
+			trackColor="#b2328c"
+			railColor="#e2e8f0"
 			value={ val }
 			onChange={ ( value ) => onChange( value ) }
 			min={ 10 }
